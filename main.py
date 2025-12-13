fb9f536520fc4abb73522feed254af688a0da0b0
Prit K Panchal <ppanchal2504@gmail.com>
2025-12-13 19:19:02 +0530

    removed ai tools

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routes.pdf_routes import router as pdf_router
 from routes.image_routes import router as image_router
-from routes.ai_routes import router as ai_router
 
 app = FastAPI(title="PDF/Image Tools API")
 
@@ -23,9 +22,3 @@ app.add_middleware(
 app.include_router(pdf_router, prefix="/pdf", tags=["PDF Tools"])
 app.include_router(image_router, prefix="/image", tags=["Image Tools"])
 
-app.include_router(ai_router, prefix="/ai", tags=["AI Tools"])
-
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
